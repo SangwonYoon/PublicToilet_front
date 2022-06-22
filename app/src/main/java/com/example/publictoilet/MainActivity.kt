83842efd0da3fdb005212b1bb0e3cc7850d8d589
SangwonYoon <yoonsw0532@naver.com>
2022-06-22 23:42:05 +0900

    위치 권한 얻는 함수 추가

--- app/src/main/java/com/example/publictoilet/MainActivity.kt
+++ app/src/main/java/com/example/publictoilet/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.publictoilet
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Base64
 import android.util.Log
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
@@ -24,14 +34,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        checkPermission()
+
+        val mapView = initMapView()
+
+        val myHome = makeMarker(37.6106656, 127.0064049, 0, "my Home")
+        mapView.addPOIItem(myHome)
+    }
+
+    private fun initMapView() : MapView{
         val mapView = MapView(this)
 
         val mapViewContainer = mapContainer
         mapViewContainer.addView(mapView)
 
-        val myHome = makeMarker(37.6106656, 127.0064049, 0)
-
-        mapView.addPOIItem(myHome)
+        return mapView
     }
 
     /**
@@ -50,5 +67,55 @@ class MainActivity : AppCompatActivity() {
         marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
 
         return marker
+    }
+
+    /**
+     * 위치 권한을 사용자로부터 얻는 함수
+     */
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !== PackageManager.PERMISSION_GRANTED)
+        {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                AlertDialog.Builder(this)
+                    .setTitle("알림")
+                    .setMessage("저장소 권한이 거부되었습니다. 사용을 원하시면 설정에서 해당 권한을 직접 허용하셔야 합니다.")
+                    .setNeutralButton("설정"
+                    ) { _, _ ->
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        intent.data = Uri.parse("package:$packageName")
+                        startActivity(intent)
+                    }
+                    .setPositiveButton("확인"
+                    ) { _, _ -> finish() }
+                    .setCancelable(false)
+                    .create()
+                    .show()
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSION_STORAGE)
+            }
+        }
+    }
+
+    /**
+     * 사용자가 위치 권한을 부여했는지 확인하는 함수
+     */
+    override fun onRequestPermissionsResult(requestCode:Int, @NonNull permissions:Array<String>, @NonNull grantResults:IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MY_PERMISSION_STORAGE -> for (i in grantResults.indices)
+            {
+                // grantResults[] : 허용된 권한은 0, 거부한 권한은 -1
+                if (grantResults[i] < 0)
+                {
+                    Toast.makeText(this@MainActivity, "해당 권한을 활성화 하셔야 합니다.", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+        }
+    }
+
+    companion object{
+        var MY_PERMISSION_STORAGE = 1000
     }
 }
