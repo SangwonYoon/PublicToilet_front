3f55d55b717a7dd3a25be75094a7ff45efd7a03f
SangwonYoon <38793142+SangwonYoon@users.noreply.github.com>
2022-07-25 11:57:57 +0900

    [refactor] 화장실 정보 화면 레이아웃 수정

---
+++ app/src/main/java/com/example/publictoilet/ToiletReviewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.publictoilet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ToiletReviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toilet_review)
+    }
+}
